43a4f6b365633c3c71c6e5f0a1518307c4595b74
Kao_Chen <kaochen2@gmail.com>
2026-02-08 19:51:24 +0100

    move onnx files

--- src/text_to_speech.py
+++ src/text_to_speech.py
@@ -7,26 +7,21 @@ from scipy.io.wavfile import write
 def init_text_to_speech():
     print(_("  Initializing Text-to-Speech..."))
     # No heavy initialization needed for Piper, but we can preload the model if desired
-    model = "fr_FR-upmc-medium.onnx"
+    model = "locales/fr/voices/fr_FR-upmc-medium.onnx"
     try:
         tts = PiperVoice.load(model)
         print(_("✅ Text-to-Speech is ready!\n"))
     except Exception as e:
         print(f"❌ Failed to load TTS model: {e}")
         sys.exit(1)
-    return 
+    return tts
 
-def synthesize_and_play(text, save_to_file=False, filename="output.wav", voice=None):
+def synthesize_and_play(text, voice=None):
     """
     Synthesize text to speech and play it via sounddevice.
-
-    Args:
-        text (str): The text to synthesize.
-        save_to_file (bool): If True, saves the result to a WAV file.
-        filename (str): Name of the output file (if save_to_file=True).
     """
-    model = "fr_FR-upmc-medium.onnx"
-
+    voice = voice if voice else init_text_to_speech();
+    
     stream = sd.OutputStream(
         samplerate=voice.config.sample_rate,
         channels=1,
@@ -43,16 +38,8 @@ def synthesize_and_play(text, save_to_file=False, filename="output.wav", voice=N
         stream.write(int_data)
         sd.wait()
 
-        # Save to file if requested
-        if save_to_file:
-            write(filename, voice.config.sample_rate, int_data)
-            print(f"File saved as {filename}")
-
     except Exception as e:
         print(f"Erreur : {e}")
     finally:
         stream.stop()
         stream.close()
-
-# Example
-#synthesize_and_play("Bonjour, ceci est un test de synthèse vocale en français.", save_to_file=True)
